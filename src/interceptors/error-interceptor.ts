cd2e6c2c4bfd84765da2f939cfd76c2b28fb77e3
Marcio Silva <marcioadsworks@gmail.com>
2020-01-09 21:42:17 -0300

    handle error 401 with alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,12 +2,13 @@ import { StorageService } from './../services/storage.service';
 import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
+import { AlertController } from 'ionic-angular';
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService,public alertCtrl: AlertController) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -28,9 +29,16 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch (errorObj.status) {
+                    case 401:
+                        this.handle401();
+                        break;
+
                     case 403:
-                    this.handle403();
-                    break;
+                        this.handle403();
+                        break;
+                    default:
+                        this.handleDefautError(errorObj);
+                        
 
                 }
                 return Observable.throw(errorObj);
@@ -40,6 +48,34 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title:'erro 401: Falha na autenticação',
+            message:'Email ou senha invalido',
+            enableBackdropDismiss: false,
+            buttons:[
+                {
+                    text:'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefautError(errorObj){
+        let alert = this.alertCtrl.create({
+            title:'erro '+errorObj.status+': '+errorObj.error,
+            message:errorObj.message,
+            enableBackdropDismiss: false,
+            buttons:[
+                {
+                    text:'Ok'
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
