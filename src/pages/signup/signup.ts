f0631ea4a5d4b5d3ff6a0286dd4f75282929e8d5
Marcio Silva <marcioadsworks@gmail.com>
2020-01-09 23:38:49 -0300

    creating form group

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder,Validators } from '@angular/forms';
 
 
 @IonicPage()
@@ -9,14 +10,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formbuilder:FormBuilder) {
+
+      this.formGroup = this.formbuilder.group({
+        nome:['marcio',[Validators.required,Validators.minLength(5),Validators.maxLength(120)]],
+        email:['marcio@gmail.com',[Validators.required,Validators.email]],
+        tipo:['1',[Validators.required]],
+        cpfOuCnpj:['06134596280',[Validators.required,Validators.minLength(11),Validators.maxLength(14)]],
+        senha:['12345',[Validators.required]],
+        logradouro:['rua 4',[Validators.required]],
+        numero:['350',[Validators.required]],
+        complemento:['ap 203',[]],
+        bairro:['pedras',[]],
+        cep:['60878050',[Validators.required]],
+        telefone1:['99878787',[Validators.required]],
+        telefone2:['',[]],
+        telefone3:['',[]],
+        estadoId:[null,[Validators.required]],
+        cidadeId:[null,[Validators.required]]
+      });
   }
 
   signupUser(){
-    console.log("signupUser")
+    console.log("signupUser");
   }
   updateCidades(){
-    
+
   }
 
 }
